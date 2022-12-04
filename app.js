f43a0f6e5650751a89c1cb26425d50c3637551d4
Chase McClain <iamchasem.dev@gmail.com>
2022-12-04 11:21:10 -0700

    completed exercises

--- app.js
+++ app.js
@@ -3,8 +3,34 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = 'Wednsday';
+let lastName = 'Addams';
+var age = 16;
 
-// Exercise 2
+lastName = 'Ortega';
+age = 20;
 
-// Exercise 3
+let language = 'JavaScript';
+let createdYear = '1995';
+let isCaseSensitive = true;
+
+console.log(`${language} was first released in ${createdYear}`)
+
+let product = 'Graphic Tee'
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectSize = 'M';
+
+console.log(`${product} in ${selectSize} is $${price} with ${stock} in stock`)
+
+let title = 'No Exit';
+let author = 'Taylor Adams'
+let pageCount = 350;
+let bookmark = 300;
+let hasRead = true;
+
+console.log(`${title} by ${author} is ${pageCount} pages long. Your last bookmark was page ${bookmark}`)
+
